fb465fd6b89221670f20108ee4d70d62a654e284
celikovicmarija <celikovicmarija@gmail.com>
2020-12-09 11:12:30 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './style.css';
+import Header from "./Header";
+import MemeGenerator from "./MemeGenerator";
+
+function App() {
+    return (
+        <div>
+            <Header/>
+            <MemeGenerator/>
+
+        </div>
+    )
+}
+
+
+export default App;
